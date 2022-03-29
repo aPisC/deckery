e362303550a173a6c1944484795f9b97c6de17af
Bendeguz Cseresnyes <wd978494@varian.com>
2022-03-29 22:23:53 +0200

    Fix Monorepo skaffold configs

--- services/game-ui/src/App.tsx
+++ services/game-ui/src/App.tsx
@@ -1,11 +1,8 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { TestInterface } from "common";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const test: TestInterface = { name: "" };
-
   return (
     <div className="App">
       <header className="App-header">
@@ -13,12 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
